79ab5ae071e33a2ed499375c81e68b4a83a83387
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-01 23:09:33 +0300

    new solutions

--- src/Solution.java
+++ src/Solution.java
@@ -1907,4 +1907,33 @@ public class Solution {
 
         return result;
     }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null || subRoot == null) return false;
+
+        if (root.val == subRoot.val && isSameTrees(root, subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTrees(TreeNode first, TreeNode second) {
+        if (first == null && second == null) return true;
+        if (first == null || second == null) return false;
+        if (first.val != second.val) return false;
+
+        return isSameTrees(first.left, second.left) && isSameTrees(first.right, second.right);
+    }
+
+    public int distributeCandies(int[] candyType) {
+        int n = candyType.length;
+        Set<Integer> types = new HashSet<>();
+        for (int j : candyType) {
+            types.add(j);
+        }
+
+        int canEat = n / 2;
+        int typesCount = types.size();
+
+        return Math.min(canEat, typesCount);
+    }
 }
