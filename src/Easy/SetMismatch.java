852b7f5ffd047f8f8e19ede7c8370f7114ddadca
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-05 13:54:19 +0300

    new solution

---
+++ src/Easy/SetMismatch.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class SetMismatch {
+    public int[] findErrorNums(int[] nums) {
+        int[] count = new int[nums.length + 1];
+
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (count[i] == 2) duplicate = i;
+            if (count[i] == 0) missing = i;
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
