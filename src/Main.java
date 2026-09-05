ce02c4bed9d4f8fa2882fb2923b947e70557fbd8
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-05 13:37:43 +0300

    new solution

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import Easy.MaximumAverageSubarray;
+
 public class Main {
     public static void main(String[] args) {
+        MaximumAverageSubarray ma = new MaximumAverageSubarray();
+        int[] nums = {
+                -1
+        };
+
+        ma.findMaxAverage(nums, 1);
     }
 }
