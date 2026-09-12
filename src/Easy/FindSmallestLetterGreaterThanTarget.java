3b130e73ef32ca427a04ac53a5e8dcc71974519b
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-12 12:24:04 +0300

    new solution

---
+++ src/Easy/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for (char letter : letters) {
+            if (letter > target) {
+                return letter;
+            }
+        }
+
+        return letters[0];
+    }
+}
