b120d6abb5afe108dbb06a3f095be95192c8711b
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-14 12:17:23 +0300

    new solution

---
+++ src/Easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,14 @@
+package Easy;
+
+import Common.TreeNode;
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
