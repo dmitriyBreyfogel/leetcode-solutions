a22f58cb892c14d79fd0cb99f0e871be20d2c2f2
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-15 12:17:21 +0300

    new solution

---
+++ src/Medium/CombinationSum.java
@@ -0,0 +1,27 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> list, int[] candidates, int remaining, int index) {
+        if (remaining == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        if (remaining < 0) return;
+
+        for (int i = index; i < candidates.length; i++) {
+            list.add(candidates[i]);
+            backtrack(result, list, candidates, remaining - candidates[i], i);
+            list.remove(list.size() - 1);
+        }
+    }
+}
