88fbf41b860f3b3f7aa691ecbd933fec92ca7fb8
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-15 11:04:18 +0300

    new solution

---
+++ src/Easy/BinaryWatch.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryWatch {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    result.add(h + ":" + String.format("%02d", m));
+                }
+            }
+        }
+
+        return result;
+    }
+}
