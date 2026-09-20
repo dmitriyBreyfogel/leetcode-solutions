e85b6028f3d4b3fb9d17441a9e589e1ca4105339
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-20 21:43:49 +0300

    new solution

---
+++ src/Medium/Permutations2.java
@@ -0,0 +1,32 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutations2 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used) {
+        if (nums.length == current.size()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(result, current, nums, used);
+            used[i] = false;
+            current.remove(current.size() - 1);
+        }
+    }
+}
