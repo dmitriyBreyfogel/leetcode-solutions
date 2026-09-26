e4838c11408472101a8ecbd518a2504851fcdcc3
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-26 12:05:08 +0300

    new solution

---
+++ src/Medium/JumpGame.java
@@ -0,0 +1,14 @@
+package Medium;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, nums[i] + i);
+            if (maxReach >= nums.length - 1) return true;
+        }
+
+        return true;
+    }
+}
